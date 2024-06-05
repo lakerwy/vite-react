f329b6f6c76e75847d9a311db0083a8574d268c8
付柏磊 <1464778543@qq.com>
2024-06-05 09:00:46 +0800

    配置tailwind.css

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import {store} from '@/store';
 import './style/index.scss'
 import './style/data-flex.css'
+import './tailwind.css'
 import { RouterProvider } from 'react-router-dom'
 import Router from './router'
 
